54958e66ff5429d7ea5337865126e7af44ded782
Guilherme Chalita <guilhermechalita@hotmail.com>
2024-02-08 17:02:42 -0300

    feat: add react-hook-form to SignIn page

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
+import './global.css'
+
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
-import './global.css'
 import { Router } from './routes'
 
 export function App() {
