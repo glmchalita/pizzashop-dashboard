2b77dc9506a1bb572028277fb442d754f866fe8c
Guilherme Chalita <guilhermechalita@hotmail.com>
2024-02-15 18:52:07 -0300

    dev: authenticate users

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme/theme-provider'
+import { queryClient } from './lib/react-query'
 import { Router } from './routes'
 
 export function App() {
@@ -12,7 +14,9 @@ export function App() {
     <HelmetProvider>
       <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | pizza.shop" />
-        <RouterProvider router={Router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={Router} />
+        </QueryClientProvider>
 
         <Toaster />
       </ThemeProvider>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import * as z from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -21,9 +23,13 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSignIn(data: SignInForm) {
     try {
-      console.log(data)
+      await authenticate({ email: data.email })
 
       toast.success('Enviamos um link de autenticação para seu e-mail.', {
         action: {
@@ -33,10 +39,8 @@ export function SignIn() {
           },
         },
       })
-
-      await new Promise((resolve) => setTimeout(resolve, 2000))
     } catch {
-      toast.success('Credenciais inválidas.', {
+      toast.error('Credenciais inválidas.', {
         description: 'Por favor verifque seus dados.',
       })
     }
